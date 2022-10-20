26b7389d7eb3b82492ba69f4491b8f1097c81862
Carlos Nogueira <carlosnogu8@gmail.com>
2022-10-20 18:55:41 -0300

    Exercício 33

---
+++ src/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+
+export const MeuHeader = styled.header`
+    background-color: orange;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 50px;
+    height: 10%;
+`
+
+export const MinhaMain = styled.main`
+    min-height: 80%;
+    display: flex;
+`
+
+export const MinhaNav = styled.nav`
+    flex-basis: 200px;
+    border-right-style: solid;
+    border-right-width: thin;
+`
